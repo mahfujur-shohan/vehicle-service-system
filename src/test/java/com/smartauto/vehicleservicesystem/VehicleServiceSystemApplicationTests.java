dc789d95243cc4508f8a4c30e25870f3196bf2e8
mahfujur-shohan <mahfujurrahmancityu@gmail.com>
2025-11-14 17:22:30 +0600

    initial setup

---
+++ src/test/java/com/smartauto/vehicleservicesystem/VehicleServiceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartauto.vehicleservicesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
